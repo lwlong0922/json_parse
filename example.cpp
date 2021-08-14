7b69daf57e76a6e59f12bccca96faa9909dcf569
ouo_lwl <80467482+lwlong0922@users.noreply.github.com>
2021-08-14 14:06:05 +0800

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,6 +1,27 @@
 #include "json.h"
 void loadFile(const string &strPath)
 {
+//[
+//	{
+//		"nId": 2001,
+//		"strName" : "药水哥",
+//		"nBirthRow" : 8,
+//		"nBirthCol" : 8,
+//		"nMapId" : 1001,
+//		"strPic" : "药",
+//		"nType" : 0
+//	},
+//	{
+//		"nId": 2002,
+//		"strName" : "武器大师",
+//		"nBirthRow" : 8,
+//		"nBirthCol" : 9,
+//		"nMapId" : 1001,
+//		"strPic" : "武",
+//		"nType" : 0
+//	}
+//]
+    
     //传入路径，生成适配器
     auto jba = new JsonAdapter(strPath);
     //拿到json对象
@@ -28,3 +49,87 @@ void loadFile(const string &strPath)
         m_vecDatas.push_back(cDt);
     }
 }
+
+
+
+
+
+
+//[
+//{
+//	"nId": 1001,
+//		"strName" : "新手村",
+//		"nMaxRow" : 20,
+//		"nMaxCol" : 15,
+//		"nBirthRow" : 5,
+//		"nBirthCol" : 5,
+//		"transfer" : [
+//	{
+//		"nRow": 11,
+//			"nCol" : 10,
+//			"nextId" : 1002
+//	}
+//		]
+//},
+//{
+//	"nId": 1002,
+//	"strName" : "高手村",
+//	"nMaxRow" : 20,
+//	"nMaxCol" : 15,
+//	"nBirthRow" : 1,
+//	"nBirthCol" : 1,
+//	"transfer" : [
+//	{
+//		"nRow": 11,
+//			"nCol" : 10,
+//			"nextId" : 1001
+//	},
+//	{
+//		"nRow": 4,
+//		"nCol" : 4,
+//		"nextId" : 1003
+//	}
+//	]
+//}
+//]
+void CMapDtMgr::loadFile(const string& strPath)
+{
+	auto aj = new JsonAdapter(strPath);
+	auto ab = aj->getJsonObject();
+	for(int i = 0; i < ab.getArraySize(); i++)
+	{
+		auto mapDt = new CMapDt();
+		mapDt->nId = ab[i].getInteger("nId");
+		mapDt->nMaxRow = ab[i].getInteger("nMaxRow");
+		mapDt->nMaxCol = ab[i].getInteger("nMaxCol");
+		mapDt->nBirthRow = ab[i].getInteger("nBirthRow");
+		mapDt->nBirthCol = ab[i].getInteger("nBirthCol");
+		mapDt->strName = ab[i].getString("strName");
+		mapDt->pArrMap = new int *[mapDt->nMaxRow];
+		for(int j = 0; j < mapDt->nMaxRow; j++)
+		{
+			mapDt->pArrMap[j] = new int[mapDt->nMaxCol];
+		}
+		for(int j = 0; j < mapDt->nMaxRow; j++)
+		{
+			for(int k = 0; k < mapDt->nMaxCol; k++)
+			{
+				if(j == 0 || k == 0 || j == mapDt->nMaxRow - 1 || k == mapDt->nMaxCol - 1)
+					mapDt->pArrMap[j][k] = 1;
+				else
+					mapDt->pArrMap[j][k] = 0;
+			}
+		}
+		auto nextObj = ab[i].getObject("transfer");
+		for(int j = 0; j < nextObj.getArraySize(); j++)
+		{
+			int nRow = nextObj[j].getInteger("nRow");
+			int nCol = nextObj[j].getInteger("nCol");
+			int nextId = nextObj[j].getInteger("nextId");
+			mapDt->pArrMap[nRow][nCol] = nextId;
+		}
+
+		m_vecDatas.push_back(mapDt);
+	}
+}
+
